93fb5b54263c7c81cf0acfafba4be73d09846078
millenium-1 <louiskaroki2010@gmail.com>
2024-07-28 15:03:10 +0300

    next commit

--- blogpr/blogpr/settings.py
+++ blogpr/blogpr/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'blogpr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templetes'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
